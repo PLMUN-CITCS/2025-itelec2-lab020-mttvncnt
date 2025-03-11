bd84e952b4bb30bd1311906a1accb8a258cd3da7
mttvncnt <florestoto99@gmail.com>
2025-03-12 00:38:24 +0800

    Submitting Python Week 05 - Laboratory # 20

--- even_odd_checker_functions.py
+++ even_odd_checker_functions.py
@@ -1,29 +1,3 @@
-score = int(input("Enter your score: "))
-
-if score >= 90:
-    grade = 'A'
-elif score >= 80:
-    grade = 'B'
-elif score >= 70:
-    grade = 'C'
-elif score >= 60:
-    grade = 'D'
-else:
-    grade = 'F'
-
-print("Your Grade is:", grade)
-John Carlo Nuqui
-11:28 PM
-n = int(input("Enter a non-negative integer: "))
-
-if n < 0:
-    print("Please enter a non-negative integer.")  
-else:
-    factorial = 1
-    for i in range(1, n + 1):  
-        factorial *= i
-
-    print(f"The factorial of {n} is: {factorial}")
 number = int(input("Enter an integer: "))
 
 if number % 2 == 0:
